90c3659d19cbbe751fb2a3aa305de60fe3c7007e
Francisco Calderon <franciscocalderon1990@gmail.com>
2022-04-27 18:16:41 -0300

    Changing header img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import "./header.css";
 function Header() {
   return (
     <>
-      <img className="header-logo" src="https://logo-logos.com/wp-content/uploads/2016/11/Game_of_Thrones_logo_black.png" alt="Logo" />
+      <img className="header-logo" src="https://3.bp.blogspot.com/-IthmPcbby_8/Woq6X7dNTwI/AAAAAAAAPIk/0q3_AOC0OuA36lLEp2so53iNKJyNY164ACLcBGAs/s640/nWc_qRgg_400x400.jpg" alt="Logo" />
       <div className="subTitle">A quick look to all the main characters of one of the most important and greatest TV series!</div>
     </>
   );
