a9980048acde6a3ae13b5c07c1d0a1c6c5dea4d9
Francisco Calderon <franciscocalderon1990@gmail.com>
2022-05-10 16:17:39 -0300

    Fixed Responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,10 @@ import "./header.css";
 function Header() {
   return (
     <>
-      <img className="header-logo" src="https://3.bp.blogspot.com/-IthmPcbby_8/Woq6X7dNTwI/AAAAAAAAPIk/0q3_AOC0OuA36lLEp2so53iNKJyNY164ACLcBGAs/s640/nWc_qRgg_400x400.jpg" alt="Logo" />
-      <div className="subTitle">A quick look to all the main characters of one of the most important and greatest TV series!</div>
+      <div className="headerContainer">
+        <img className="header-logo" src="https://3.bp.blogspot.com/-IthmPcbby_8/Woq6X7dNTwI/AAAAAAAAPIk/0q3_AOC0OuA36lLEp2so53iNKJyNY164ACLcBGAs/s640/nWc_qRgg_400x400.jpg" alt="Logo" />
+        <div className="subTitle">A quick look to all the main characters of one of the most important and greatest TV series!</div>
+      </div>
     </>
   );
 }
